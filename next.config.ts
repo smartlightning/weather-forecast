69954758308a34af14c9e32162fe2198d29efe42
chrismcqueen <chris.mcqueen@bergfreunde.de>
2025-01-15 09:21:33 +0100

    Show forecast data

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['openweathermap.org'], // Allow OpenWeatherMap images
+  },
 };
 
 export default nextConfig;
